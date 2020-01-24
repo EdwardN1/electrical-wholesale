a6295bd608b241d54c0d40727b001092dcc316f3
EdwardN1 <edward@technicks.com>
2020-01-24 20:41:19 +0000

    Fonts and SCSS

--- assets/styles/dynamic-css.php
+++ assets/styles/dynamic-css.php
@@ -4,10 +4,12 @@ header("Content-type: text/css; charset: UTF-8");
 require_once get_template_directory().'/functions/scssphp/scss.inc.php';
 use ScssPhp\ScssPhp\Compiler;
 $scss = new Compiler();
-echo $scss->compile('$body-font-size: 20px;
-body {
-  font-size: $body-font-size;
+$includes = '';
+define('INCLUDESSPATH', get_template_directory() . '/assets/scss/dynamic/');
+foreach (glob(INCLUDESSPATH.'_*.scss') as $filename)
+{
+	$includes .= file_get_contents ($filename);
 }
-');
+echo $scss->compile($includes);
 
 
