405c7adba499e9a6c2248527c2ffe62001f038e0
EdwardN1 <GH?841241ed>
2020-01-24 14:08:48 +0000

    Setup Dynamic CSS

--- assets/styles/dynamic-css.php
+++ assets/styles/dynamic-css.php
@@ -1,8 +1,13 @@
 <?php
 header("Content-type: text/css; charset: UTF-8");
 
-?>
-
+require_once get_template_directory().'/functions/scssphp/scss.inc.php';
+use ScssPhp\ScssPhp\Compiler;
+$scss = new Compiler();
+echo $scss->compile('$body-font-size: 20px;
 body {
-font-size: 20px;
+  font-size: $body-font-size;
 }
+');
+
+
--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -6,6 +6,50 @@
  */
 ?>
 
+
+<?php $mobile_break_point = get_field( 'mobile_break_point', 'option' ); ?>
+<?php if ( have_rows( 'header_rows', 'option' ) ) : ?>
+    <?php while ( have_rows( 'header_rows', 'option' ) ) : the_row(); ?>
+        <?php $row_name = get_sub_field( 'row_name' ); ?>
+        <?php $row_type = get_sub_field( 'row_type' ); ?>
+        <?php $row_height = get_sub_field( 'row_height' ); ?>
+        <?php $row_height_mobile = get_sub_field( 'row_height_mobile' ); ?>
+        <?php $row_max_height = get_sub_field( 'row_max_height' ); ?>
+        <?php $row_max_height_mobile = get_sub_field( 'row_max_height_mobile' ); ?>
+        <?php $row_background_type = get_sub_field( 'row_background_type' ); ?>
+        <?php $row_background_image = get_sub_field( 'row_background_image' ); ?>
+        <?php $row_background_image_URL = $row_background_image['url']; ?>
+        <?php $row_background_image_ALT = $row_background_image['alt']; ?>
+        <?php $row_background_colour = get_sub_field( 'row_background_colour' ); ?>
+        <?php if ( have_rows( 'container_rows' ) ) : ?>
+            <?php while ( have_rows( 'container_rows' ) ) : the_row(); ?>
+                <?php if ( have_rows( 'container_columns' ) ) : ?>
+                    <?php while ( have_rows( 'container_columns' ) ) : the_row(); ?>
+                        <?php the_sub_field( 'container_name' ); ?>
+                        <?php the_sub_field( 'column_width' ); ?>
+                        <?php the_sub_field( 'container_content_type' ); ?>
+                        <?php the_sub_field( 'container_content' ); ?>
+                        <?php the_sub_field( 'container_content_text' ); ?>
+                        <pre><code><?php echo esc_html( get_sub_field( 'container_css' ) ); ?></code></pre>
+                        <pre><code><?php echo esc_html( get_sub_field( 'container_css_mobile' ) ); ?></code></pre>
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <?php // no rows found ?>
+                <?php endif; ?>
+            <?php endwhile; ?>
+        <?php else : ?>
+            <?php // no rows found ?>
+        <?php endif; ?>
+        <?php the_sub_field( 'content_type' ); ?>
+        <?php the_sub_field( 'wysiwyg_content' ); ?>
+        <?php the_sub_field( 'text_content' ); ?>
+        <pre><code><?php echo esc_html( get_sub_field( 'row_css' ) ); ?></code></pre>
+        <pre><code><?php echo esc_html( get_sub_field( 'row_css_mobile' ) ); ?></code></pre>
+    <?php endwhile; ?>
+<?php else : ?>
+    <?php // no rows found ?>
+<?php endif; ?>
+
 <div class="top-bar" id="top-bar-menu">
 	<div class="top-bar-left float-left">
 		<ul class="menu">
