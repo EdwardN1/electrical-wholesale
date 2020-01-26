e43b6abc59d27049992ba710bd6b064e19eedea6
EdwardN1 <GH?841241ed>
2020-01-26 18:41:08 +0000

    Layout Settings

---
+++ functions/includes/autoinc-shortcodes.php
@@ -0,0 +1,160 @@
+<?php
+add_shortcode('icon', 'Generateicon');
+add_shortcode('nav', 'Generatenav');
+
+function Generatenav($params = array())
+{
+
+    // default parameters
+    extract(shortcode_atts(array(
+        'name' => '',
+        'includelink' => false,
+        'includeheading' => false
+    ), $params));
+
+    /**
+     *
+     * Need to tell IDE that these variables do exist
+     *
+     * @var string $name
+     * @var string $align
+     * @var string $stretch
+     */
+
+    $res = '';
+
+    if($name='The Main Menu') {
+        ob_start();
+        joints_top_nav();
+        $res = ob_get_contents();
+        ob_end_clean();
+        return $res;
+        error_log('menu - '.$res);
+    }
+
+    if($name='The Secondary Menu') {
+        ob_start();
+        joints_secondary_nav();
+        $res = ob_get_contents();
+        ob_end_clean();
+    }
+
+    if($name='The Off-Canvas Menu (Mobile)') {
+        ob_start();
+        joints_off_canvas_nav();
+        $res = ob_get_contents();
+        ob_end_clean();
+    }
+
+    if($name='Category Menu') {
+        ob_start();
+        joints_category_menu();
+        $res = ob_get_contents();
+        ob_end_clean();
+    }
+
+    if($name='Links Menu') {
+        ob_start();
+        joints_links_menu();
+        $res = ob_get_contents();
+        ob_end_clean();
+    }
+
+    return $res;
+}
+
+function Generateicon($params = array())
+{
+
+    // default parameters
+    extract(shortcode_atts(array(
+        'name' => '',
+        'includelink' => false,
+        'includeheading' => false
+    ), $params));
+
+    /**
+     *
+     * Need to tell IDE that these variables do exist
+     *
+     * @var string $includelink
+     * @var string $name
+     * @var string $includeheading
+     */
+
+    $res = '';
+    if($name) {
+        if (have_rows('icons', 'option')) :
+            while (have_rows('icons', 'option')) : the_row();
+                $iname = get_sub_field('name');
+                $icon = get_sub_field('icon');
+                if ($iname == $name) {
+                    //error_log('$iname = '.$iname.' | $name = '.$name);
+                    if ($icon) {
+                        $iconURL = $icon['url'];
+                        $iconALT = $icon['alt'];
+                        $link = get_sub_field('link');
+                        $heading = get_sub_field('heading');
+                        $heading_position = get_sub_field('heading_position');
+                        $iconSRC = '<img src="'.$iconURL.'" alt="'.$iconALT.'" class="icon-'.$name.'"/>';
+                        $iconLink = '<a href="'.$link.'">'.$iconSRC.'</a>';
+                        //error_log('$iconSRC = '.$iconSRC);
+                       // error_log('$iconLink = '.$iconLink);
+                        if($includeheading=='true'){
+                            if($heading_position == 'top') {
+                                if($includelink=='true') {
+                                    $res = '<div class="icon heading-top"><div class="icon-heading">'.$heading.'</div><div>'.$iconLink.'</div></div>';
+                                } else {
+                                    $res = '<div class="icon heading-top"><div class="icon-heading">'.$heading.'</div><div>'.$iconSRC.'</div></div>';
+                                }
+                            }
+                            if($heading_position == 'Bottom') {
+                                if($includelink=='true') {
+                                    $res = '<div class="icon heading-bottom"><div>'.$iconLink.'</div><div class="icon-heading">'.$heading.'</div></div>';
+                                } else {
+                                    $res = '<div class="icon heading-bottom"><div>'.$iconSRC.'</div><div class="icon-heading">'.$heading.'</div></div>';
+                                }
+                            }
+                            if($heading_position == 'Left') {
+                                if($includelink=='true') {
+                                    $res = '<div class="icon heading-left grid-x"><div class="cell auto icon-heading">'.$heading.'</div><div class="cell shrink">'.$iconLink.'</div></div>';
+                                } else {
+                                    $res = '<div class="icon heading-left grid-x"><div class="cell auto icon-heading">'.$heading.'</div><div class="cell shrink">'.$iconSRC.'</div></div>';
+                                }
+                            }
+                            if($heading_position == 'Right') {
+                                if($includelink=='true') {
+                                    $res = '<div class="icon heading-right grid-x"><div class="cell shrink">'.$iconLink.'</div><div class="cell auto icon-heading">'.$heading.'</div></div>';
+                                } else {
+                                    $res = '<div class="icon heading-right grid-x"><div class="cell shrink">'.$iconSRC.'</div><div class="cell auto icon-heading">'.$heading.'</div></div>';
+                                }
+                            }
+                        } else {
+                            if($includelink=='true') {
+                                $res = $iconLink;
+                            } else {
+                                $res = $iconSRC;
+                            }
+                            //error_log($res);
+                        }
+                    } else {
+                        $res = $name . ' - Please add an image to this icon to display it.';
+                    }
+                    break;
+                }
+            endwhile;
+            reset_rows();
+        else :
+            $res = 'No icons created. Go set some up in Theme Settings';
+        endif;
+    } else {
+        $res = 'No name set - cannot show the icon';
+    }
+
+    if($res=='') {
+        $res = $name.' - Could not find that name in the icons list. Check the spelling or go setup the icon in Theme Settings.';
+    }
+    //error_log($res);
+    return $res;
+}
+
--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -1,68 +1,45 @@
-<?php
-/**
- * The off-canvas menu uses the Off-Canvas Component
- *
- * For more info: http://jointswp.com/docs/off-canvas-menu/
- */
-?>
-
-
-<?php $mobile_break_point = get_field( 'mobile_break_point', 'option' ); ?>
-<?php if ( have_rows( 'header_rows', 'option' ) ) : ?>
-    <?php while ( have_rows( 'header_rows', 'option' ) ) : the_row(); ?>
-        <?php $row_name = get_sub_field( 'row_name' ); ?>
-        <?php $row_type = get_sub_field( 'row_type' ); ?>
-        <?php $row_height = get_sub_field( 'row_height' ); ?>
-        <?php $row_height_mobile = get_sub_field( 'row_height_mobile' ); ?>
-        <?php $row_max_height = get_sub_field( 'row_max_height' ); ?>
-        <?php $row_max_height_mobile = get_sub_field( 'row_max_height_mobile' ); ?>
-        <?php $row_background_type = get_sub_field( 'row_background_type' ); ?>
-        <?php $row_background_image = get_sub_field( 'row_background_image' ); ?>
-        <?php $row_background_image_URL = $row_background_image['url']; ?>
-        <?php $row_background_image_ALT = $row_background_image['alt']; ?>
-        <?php $row_background_colour = get_sub_field( 'row_background_colour' ); ?>
-        <?php if ( have_rows( 'container_rows' ) ) : ?>
-            <?php while ( have_rows( 'container_rows' ) ) : the_row(); ?>
-                <?php if ( have_rows( 'container_columns' ) ) : ?>
-                    <?php while ( have_rows( 'container_columns' ) ) : the_row(); ?>
-                        <?php the_sub_field( 'container_name' ); ?>
-                        <?php the_sub_field( 'column_width' ); ?>
-                        <?php the_sub_field( 'container_content_type' ); ?>
-                        <?php the_sub_field( 'container_content' ); ?>
-                        <?php the_sub_field( 'container_content_text' ); ?>
-                        <pre><code><?php echo esc_html( get_sub_field( 'container_css' ) ); ?></code></pre>
-                        <pre><code><?php echo esc_html( get_sub_field( 'container_css_mobile' ) ); ?></code></pre>
-                    <?php endwhile; ?>
-                <?php else : ?>
-                    <?php // no rows found ?>
+<?php if (have_rows('header_rows', 'option')) : ?>
+    <?php while (have_rows('header_rows', 'option')) : the_row(); ?>
+        <div class="<?php echo cssName(get_sub_field('row_name')); ?>">
+            <div class="grid-container">
+                <?php $row_type = get_sub_field('row_type'); ?>
+                <?php if ($row_type == 'Container'): ?>
+                    <?php if (have_rows('container_rows')) : ?>
+                        <?php while (have_rows('container_rows')) : the_row(); ?>
+                            <div class="grid-x <?php echo cssName(get_sub_field('container_row_name')); ?>">
+                                <?php if (have_rows('container_columns')) : ?>
+                                    <?php while (have_rows('container_columns')) : the_row(); ?>
+                                        <div class="<?php echo cssName(get_sub_field('container_name')); ?> cell <?php the_sub_field('column_width'); ?>">
+                                            <?php $container_content_type = get_sub_field('container_content_type'); ?>
+                                            <?php if ($container_content_type == 'Wysiwyg Content'): ?>
+                                                <?php $container_content = the_sub_field('container_content'); ?>
+                                            <?php else: ?>
+                                                <?php the_sub_field('container_content_text'); ?>
+                                            <?php endif; ?>
+                                        </div>
+                                    <?php endwhile; ?>
+                                <?php else : ?>
+                                    <?php // no rows found ?>
+                                <?php endif; ?>
+                            </div>
+                        <?php endwhile; ?>
+                    <?php else : ?>
+                        <?php // no rows found ?>
+                    <?php endif; ?>
+                <?php else: ?>
+                    <?php $content_type = get_sub_field('content_type'); ?>
+                    <?php if ($content_type == 'Wysiwyg Editor'): ?>
+                        <?php the_sub_field('wysiwyg_content'); ?>
+                    <?php else: ?>
+                        <?php the_sub_field('text_content'); ?>
+                    <?php endif; ?>
                 <?php endif; ?>
-            <?php endwhile; ?>
-        <?php else : ?>
-            <?php // no rows found ?>
-        <?php endif; ?>
-        <?php the_sub_field( 'content_type' ); ?>
-        <?php the_sub_field( 'wysiwyg_content' ); ?>
-        <?php the_sub_field( 'text_content' ); ?>
-        <pre><code><?php echo esc_html( get_sub_field( 'row_css' ) ); ?></code></pre>
-        <pre><code><?php echo esc_html( get_sub_field( 'row_css_mobile' ) ); ?></code></pre>
+            </div>
+        </div>
     <?php endwhile; ?>
 <?php else : ?>
     <?php // no rows found ?>
 <?php endif; ?>
 
-<div class="top-bar" id="top-bar-menu">
-	<div class="top-bar-left float-left">
-		<ul class="menu">
-			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
-		</ul>
-	</div>
-	<div class="top-bar-right show-for-medium">
-		<?php joints_top_nav(); ?>	
-	</div>
-	<div class="top-bar-right float-right show-for-small-only">
-		<ul class="menu">
-			<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
-			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
-		</ul>
-	</div>
-</div>
+
+
