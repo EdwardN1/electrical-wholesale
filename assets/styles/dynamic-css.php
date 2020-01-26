a5029a8a99ab3001002f862e61735a52c19d32d8
EdwardN1 <edward@technicks.com>
2020-01-26 09:45:15 +0000

    Dynamic SCSS and Options

--- assets/styles/dynamic-css.php
+++ assets/styles/dynamic-css.php
@@ -3,13 +3,38 @@ header("Content-type: text/css; charset: UTF-8");
 
 require_once get_template_directory().'/functions/scssphp/scss.inc.php';
 use ScssPhp\ScssPhp\Compiler;
-$scss = new Compiler();
-$includes = '';
-define('INCLUDESSPATH', get_template_directory() . '/assets/scss/dynamic/');
+
+function scss($scss) {
+	$x_scss = new Compiler();
+	try {
+		return $x_scss->compile($scss);
+	} catch (Exception $e) {
+		error_log('Caught Exception: '. $e->getMessage());
+		return false;
+	}
+}
+
+define('INCLUDESSPATH', get_template_directory() . '/assets/styles/scss/includes/');
+
+
 foreach (glob(INCLUDESSPATH.'_*.scss') as $filename)
 {
+	$includes = '';
+	ob_start();
+	include $filename.'.php';
+	$includes .= preg_replace('/\s+/S', " ", ob_get_contents());
+	ob_end_clean();
 	$includes .= file_get_contents ($filename);
+	error_log($includes);
 }
-echo $scss->compile($includes);
+
+$default_cssscss = get_field( 'default_cssscss', 'option' );
+$scss = scss($default_cssscss);
+if($scss) {
+	$css .= $scss;
+}
+
+echo $css;
+
 
 
--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -4,9 +4,12 @@ function site_scripts() {
         
     // Adding scripts file in the footer
     wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/scripts/scripts.js', array( 'jquery' ), filemtime(get_template_directory() . '/assets/scripts/js'), true );
-   
+
+	// Register fonts
+	wp_enqueue_style( 'font-css', get_template_directory_uri() . '/assets/styles/scss/fonts/style.css', array(), 1.0, 'all' );
+
     // Register main stylesheet
-    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/styles/style.css', array(), filemtime(get_template_directory() . '/assets/styles/scss'), 'all' );
+    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/styles/style.css', 'font-css', filemtime(get_template_directory() . '/assets/styles/scss'), 'all' );
 
     // Register dynamic stylesheet
     wp_enqueue_style('dynamic-css', admin_url('admin-ajax.php') . '?action=dynamic_css', 'site-css',  '1.0', 'all');
