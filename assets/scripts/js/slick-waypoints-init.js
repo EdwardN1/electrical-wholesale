5ceb01f3bc922304fa5330cdc5f756ac0b0c2c06
EdwardN1 <edward@technicks.com>
2020-02-09 13:33:55 +0000

    Carousel

--- assets/scripts/js/slick-waypoints-init.js
+++ assets/scripts/js/slick-waypoints-init.js
@@ -7,8 +7,8 @@ jQuery(document).ready(function ($) {
         var wpClass = $element.data('waypoint-class');
 
         $element.waypoint(function (direction) {
-            if(direction==wpDirection) {
-                if(!$element.hasClass(wpClass)) {
+            if (direction == wpDirection) {
+                if (!$element.hasClass(wpClass)) {
                     $element.addClass(wpClass);
                 }
             }
@@ -23,5 +23,38 @@ jQuery(document).ready(function ($) {
     };
 
     $('[data-slick-slider]').slick(slickOpts);
+
+    $('[data-slick-carousel]').each(function (e) {
+        let breakpoints = $(this).data('breakpoints');
+        let dataBP = breakpoints.split(',');
+        let X_slidesToShow = 1;
+        let X_slidesToScroll = 1;
+        let X_dots = true;
+        let X_responsive = [];
+        let tbreakpoint = {};
+        dataBP.forEach(function (BP) {
+            let sBP = BP.split(':');
+            tbreakpoint = {
+                breakpoint: parseInt(sBP[0]),
+                settings: {
+                    slidesToShow: parseInt(sBP[1]),
+                    slidesToScroll: parseInt(sBP[1])
+                }
+            };
+            X_responsive.push(tbreakpoint);
+            //window.console.log(tbreakpoint);
+        });
+
+        let carouselOpts = {
+            dots: X_dots,
+            slidesToShow: X_slidesToShow,
+            slidesToScroll: 1,
+            mobileFirst:true,
+            responsive: X_responsive
+        };
+
+        $(this).slick(carouselOpts);
+    });
+
 
 });
