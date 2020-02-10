182c206828789fdf34f3c2605e85cf7f9dc25f73
EdwardN1 <edward@technicks.com>
2020-02-10 19:31:19 +0000

    Woocommerce

--- functions/includes/autoinc-woocommerce-support.php
+++ functions/includes/autoinc-woocommerce-support.php
@@ -27,3 +27,47 @@ add_theme_support( 'wc-product-gallery-slider' );
 }
 
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );*/
+
+/*add_filter( 'woocommerce_enqueue_styles', '__return_false' );*/
+
+
+/**
+ *
+ * Rearrange woocommerce_single_product_summary
+ *
+ * Hook: woocommerce_single_product_summary.
+ *
+ * @hooked woocommerce_template_single_title - 5
+ * @hooked woocommerce_template_single_rating - 10
+ * @hooked woocommerce_template_single_price - 10
+ * @hooked woocommerce_template_single_excerpt - 20
+ * @hooked woocommerce_template_single_add_to_cart - 30
+ * @hooked woocommerce_template_single_meta - 40
+ * @hooked woocommerce_template_single_sharing - 50
+ * @hooked WC_Structured_Data::generate_product_data() - 60
+ *
+ * @hooked woocommerce_template_single_title - 5
+ * @hooked woocommerce_template_single_meta - 10
+ * @hooked woocommerce_template_single_rating - 20
+ * @hooked woocommerce_template_single_price - 20
+ * @hooked woocommerce_template_single_excerpt -30
+ * @hooked woocommerce_template_single_add_to_cart - 40
+ * @hooked woocommerce_template_single_sharing - 50
+ * @hooked WC_Structured_Data::generate_product_data() - 60
+ */
+
+remove_action('woocommerce_single_product_summary','woocommerce_template_single_title',5);
+remove_action('woocommerce_single_product_summary','woocommerce_template_single_rating',10);
+remove_action('woocommerce_single_product_summary','woocommerce_template_single_price',10);
+remove_action('woocommerce_single_product_summary','woocommerce_template_single_excerpt',20);
+remove_action('woocommerce_single_product_summary','woocommerce_template_single_add_to_cart',30);
+remove_action('woocommerce_single_product_summary','woocommerce_template_single_meta',40);
+remove_action('woocommerce_single_product_summary','woocommerce_template_single_sharing',50);
+
+add_action('woocommerce_single_product_summary','woocommerce_template_single_title',5);
+add_action('woocommerce_single_product_summary','woocommerce_template_single_meta',10);
+add_action('woocommerce_single_product_summary','woocommerce_template_single_rating',20);
+add_action('woocommerce_single_product_summary','woocommerce_template_single_price',20);
+add_action('woocommerce_single_product_summary','woocommerce_template_single_excerpt',30);
+add_action('woocommerce_single_product_summary','woocommerce_template_single_add_to_cart',40);
+add_action('woocommerce_single_product_summary','woocommerce_template_single_sharing',50);
--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -10,8 +10,8 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @package 	WooCommerce/Templates
+ * @see        https://docs.woocommerce.com/document/template-structure/
+ * @package    WooCommerce/Templates
  * @version     1.6.4
  */
 
@@ -21,7 +21,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header( 'shop' ); ?>
 
-	<?php
+    <div class="grid-container">
+		<?php
 		/**
 		 * woocommerce_before_main_content hook.
 		 *
@@ -29,7 +30,7 @@ get_header( 'shop' ); ?>
 		 * @hooked woocommerce_breadcrumb - 20
 		 */
 		do_action( 'woocommerce_before_main_content' );
-	?>
+		?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
@@ -37,23 +38,25 @@ get_header( 'shop' ); ?>
 
 		<?php endwhile; // end of the loop. ?>
 
-	<?php
+		<?php
 		/**
 		 * woocommerce_after_main_content hook.
 		 *
 		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
 		 */
 		do_action( 'woocommerce_after_main_content' );
-	?>
+		?>
 
-	<?php
+		<?php
 		/**
 		 * woocommerce_sidebar hook.
 		 *
 		 * @hooked woocommerce_get_sidebar - 10
 		 */
-		do_action( 'woocommerce_sidebar' );
-	?>
+		//do_action( 'woocommerce_sidebar' );
+		?>
+
+    </div>
 
 <?php get_footer( 'shop' );
 
