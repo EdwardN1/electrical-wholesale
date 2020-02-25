542472b07d8c07b5425913a8f82ddd2935bde91b
EdwardN1 <edward@technicks.com>
2020-02-25 20:52:07 +0000

    HFHC

--- functions/includes/autoinc-save-post.php
+++ functions/includes/autoinc-save-post.php
@@ -1,114 +1,135 @@
 <?php
-add_action('acf/save_post', 'acf_save_post_processing', 20);
+add_action( 'acf/save_post', 'acf_save_post_processing', 20 );
 
-function acf_save_post_processing($post_id)
-{
-    $screen = get_current_screen();
-    //error_log('Screen ID = '.$screen->id);
-    if (strpos($screen->id, 'theme-general-settings') == true) {
-        $header = getHeaderSCSS();
-        $footer = getFooterSCSS();
-        $typography = getTypographySCSS();
-        $defaultSCSS = getDefaultSCSS();
-        $wooSCSS = getWooSCSS();
-        file_put_contents(get_template_directory() . '/assets/styles/server/theme-settings/' . '_typography.scss', $typography);
-        file_put_contents(get_template_directory() . '/assets/styles/server/theme-settings/' . '_header.scss', $header);
-        file_put_contents(get_template_directory() . '/assets/styles/server/theme-settings/' . '_footer.scss', $footer);
-        file_put_contents(get_template_directory() . '/assets/styles/server/theme-settings/' . '_default.scss', $defaultSCSS);
-	    file_put_contents(get_template_directory() . '/assets/styles/server/theme-settings/' . '_woo.scss', $wooSCSS);
-        update_css();
-    }
+function acf_save_post_processing( $post_id ) {
+	$screen = get_current_screen();
+	//error_log('Screen ID = '.$screen->id);
+	if ( strpos( $screen->id, 'theme-general-settings' ) == true ) {
+		$header      = getHeaderSCSS();
+		$footer      = getFooterSCSS();
+		$typography  = getTypographySCSS();
+		$defaultSCSS = getDefaultSCSS();
+		$wooSCSS     = getWooSCSS();
+		file_put_contents( get_template_directory() . '/assets/styles/server/theme-settings/' . '_typography.scss', $typography );
+		file_put_contents( get_template_directory() . '/assets/styles/server/theme-settings/' . '_header.scss', $header );
+		file_put_contents( get_template_directory() . '/assets/styles/server/theme-settings/' . '_footer.scss', $footer );
+		file_put_contents( get_template_directory() . '/assets/styles/server/theme-settings/' . '_default.scss', $defaultSCSS );
+		file_put_contents( get_template_directory() . '/assets/styles/server/theme-settings/' . '_woo.scss', $wooSCSS );
+		update_css();
+	}
 }
 
-add_action('save_post', 'css_save_post', 10, 3);
+add_action( 'save_post', 'css_save_post', 10, 3 );
 
-function css_save_post($post_id, $post, $update)
-{
-    // Autosave, do nothing
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+function css_save_post( $post_id, $post, $update ) {
+	// Autosave, do nothing
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
 // AJAX? Not used here
-    if (defined('DOING_AJAX') && DOING_AJAX) return;
+	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+		return;
+	}
 // Check user permissions
-    if (!current_user_can('edit_post', $post_id)) return;
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
 // Return if it's a post revision
-    if (false !== wp_is_post_revision($post_id)) return;
+	if ( false !== wp_is_post_revision( $post_id ) ) {
+		return;
+	}
 
-    $postSCSS = '';
-    $scssFilename = '';
-    $scssImport = '';
-    $z_postSCSS = '';
+	$postSCSS     = '';
+	$scssFilename = '';
+	$scssImport   = '';
+	$z_postSCSS   = '';
 
-    if ($post->post_type == 'post') {
-        $z_postSCSS .= '.postid-' . $post_id . ' {';
-        $scssFilename = '_postid-' . $post_id . '.scss';
-        $scssImport = '@import "postid-' . $post_id . '";';
-    }
-    if ($post->post_type == 'page') {
-        $z_postSCSS .= '.page-id-' . $post_id . ' {';
-        $scssFilename = '_page-id-' . $post_id . '.scss';
-        $scssImport = '@import "page-id-' . $post_id . '";';
-    }
+	if ( $post->post_type == 'post' ) {
+		$z_postSCSS   .= '.postid-' . $post_id . ' {';
+		$scssFilename = '_postid-' . $post_id . '.scss';
+		$scssImport   = '@import "postid-' . $post_id . '";';
+	}
+	if ( $post->post_type == 'page' ) {
+		$z_postSCSS   .= '.page-id-' . $post_id . ' {';
+		$scssFilename = '_page-id-' . $post_id . '.scss';
+		$scssImport   = '@import "page-id-' . $post_id . '";';
+	}
 
-    $blocks = parse_blocks($post->post_content);
-    foreach ($blocks as $block) {
-        if ($block['blockName'] == 'acf/acfgbcblockherosection') {
-            $heroSection = $block['attrs']['data'];
-            $sliderID = cssName($heroSection['id']);
-            $sliderSCSS = $heroSection['cssscss'];
-            if($sliderSCSS!='') {
-                if($postSCSS=='') {
-                    $postSCSS .= $z_postSCSS;
-                }
-                $postSCSS .= '#'.$sliderID.' {';
-                $postSCSS .= $sliderSCSS;
-            }
-            $num_slides = $heroSection['slides'];
-            for ($x = 0; $x <= $num_slides; $x++) {
-                $slideSCSS = $heroSection['slides_'.$x.'_cssscss'];
-                if($slideSCSS!='') {
-                    if($postSCSS=='') {
-                        $postSCSS .= $z_postSCSS.'#'.$sliderID.' {';
-                    }
-                    $postSCSS .= '.slide-'.$x.' {';
-                    $postSCSS .= $slideSCSS;
-                    $postSCSS .= '}'; //.slide-number
-                }
-            }
-            if($sliderSCSS!='') {
-                $postSCSS .= '}'; //#sliderID
-            }
-            //error_log(print_r($block['attrs']['data'], true));
-        }
+	$blocks = parse_blocks( $post->post_content );
+	foreach ( $blocks as $block ) {
+		if ( $block['blockName'] == 'acf/acfgbcblockherosection' ) {
+			$heroSection = $block['attrs']['data'];
+			$sliderID    = cssName( $heroSection['id'] );
+			$sliderSCSS  = $heroSection['cssscss'];
+			if ( $sliderSCSS != '' ) {
+				if ( $postSCSS == '' ) {
+					$postSCSS .= $z_postSCSS;
+				}
+				$postSCSS .= '#' . $sliderID . ' {';
+				$postSCSS .= $sliderSCSS;
+			}
+			$num_slides = $heroSection['slides'];
+			for ( $x = 0; $x <= $num_slides; $x ++ ) {
+				$slideSCSS = $heroSection[ 'slides_' . $x . '_cssscss' ];
+				if ( $slideSCSS != '' ) {
+					if ( $postSCSS == '' ) {
+						$postSCSS .= $z_postSCSS . '#' . $sliderID . ' {';
+					}
+					$postSCSS .= '.slide-' . $x . ' {';
+					$postSCSS .= $slideSCSS;
+					$postSCSS .= '}'; //.slide-number
+				}
+			}
+			if ( $sliderSCSS != '' ) {
+				$postSCSS .= '}'; //#sliderID
+			}
+			//error_log(print_r($block['attrs']['data'], true));
+		}
 
-        if ($block['blockName'] == 'acf/acfgbcblockimagegridlinks') {
-            $section = $block['attrs']['data'];
-            $sectionID = cssName($section['id']);
-            $sectionSCSS = $section['cssscss'];
-            if($sectionSCSS!='') {
-                if($postSCSS=='') {
-                    $postSCSS .= $z_postSCSS;
-                }
-                $postSCSS .= '#'.$sectionID.' {';
-                $postSCSS .= $sectionSCSS;
-            }
+		if ( $block['blockName'] == 'acf/acfgbcblockimagegridlinks' ) {
+			$section     = $block['attrs']['data'];
+			$sectionID   = cssName( $section['id'] );
+			$sectionSCSS = $section['cssscss'];
+			if ( $sectionSCSS != '' ) {
+				if ( $postSCSS == '' ) {
+					$postSCSS .= $z_postSCSS;
+				}
+				$postSCSS .= '#' . $sectionID . ' {';
+				$postSCSS .= $sectionSCSS;
+			}
 
-            if($sectionSCSS!='') {
-                $postSCSS .= '}'; //#sliderID
-            }
-        }
-    }
+			if ( $sectionSCSS != '' ) {
+				$postSCSS .= '}'; //#sliderID
+			}
+		}
+	}
 
-    if($postSCSS!='') {
-        $postSCSS .= '}'; //#postID
-    }
+	if ( have_rows( 'content', $post_id ) ):
+		while ( have_rows( 'content', $post_id ) ): the_row();
+			$sectionID = cssName( get_sub_field( 'id' ) );
+			$sectionSCSS = get_sub_field('cssscss');
+			if($sectionSCSS != '') {
+				if ( $postSCSS == '' ) {
+					$postSCSS .= $z_postSCSS;
+				}
+				$postSCSS .= '#'.$sectionID.' {';
+				$postSCSS .= $sectionSCSS;
+				$postSCSS .= '}';
+				}
+		endwhile;
+	endif;
 
-    if($postSCSS != '') {
-        file_put_contents(get_template_directory() . '/assets/styles/server/posts/' . $scssFilename, $postSCSS);
-        $importFile = file_get_contents(get_template_directory() . '/assets/styles/server/posts/posts.scss');
-        if (strpos($importFile, $scssImport) === false) {
-            $importFile .= $scssImport;
-            file_put_contents(get_template_directory() . '/assets/styles/server/posts/posts.scss', $importFile);
-        }
-        update_css();
-    }
+	if ( $postSCSS != '' ) {
+		$postSCSS .= '}'; //#postID
+	}
+
+	if ( $postSCSS != '' ) {
+		file_put_contents( get_template_directory() . '/assets/styles/server/posts/' . $scssFilename, $postSCSS );
+		$importFile = file_get_contents( get_template_directory() . '/assets/styles/server/posts/posts.scss' );
+		if ( strpos( $importFile, $scssImport ) === false ) {
+			$importFile .= $scssImport;
+			file_put_contents( get_template_directory() . '/assets/styles/server/posts/posts.scss', $importFile );
+		}
+		update_css();
+	}
 }
--- parts/loop-page.php
+++ parts/loop-page.php
@@ -4,20 +4,31 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
-						
-	<!--<header class="article-header">
-		<h1 class="page-title"><?php /*the_title(); */?></h1>
+<article id="post-<?php the_ID(); ?>" <?php post_class( '' ); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
+
+    <!--<header class="article-header">
+		<h1 class="page-title"><?php /*the_title(); */ ?></h1>
 	</header>--> <!-- end article header -->
-					
+
     <section class="entry-content" itemprop="text">
-	    <?php the_content(); ?>
-	</section> <!-- end article section -->
-						
-	<footer class="article-footer">
-		 <?php wp_link_pages(); ?>
-	</footer> <!-- end article footer -->
-						    
+		<?php the_content(); ?>
+    </section> <!-- end article section -->
+
+    <section class="acf-content-loop">
+		<?php if ( have_rows( 'content' ) ) : ?>
+			<?php while ( have_rows( 'content' ) ) : the_row(); ?>
+                <div id="<?php echo cssName( get_sub_field( 'id' ) ); ?>"><?php the_sub_field( 'html' ); ?></div>
+			<?php endwhile; ?>
+		<?php else : ?>
+			<?php // no rows found ?>
+		<?php endif; ?>
+
+    </section>
+
+    <footer class="article-footer">
+		<?php wp_link_pages(); ?>
+    </footer> <!-- end article footer -->
+
 	<?php comments_template(); ?>
-					
+
 </article> <!-- end article -->
