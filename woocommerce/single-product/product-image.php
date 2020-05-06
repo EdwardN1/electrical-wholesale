1b7746e62248ec72d2d5c82ed76c5d3dc7fbbed6
EdwardN1 <GH?841241ed>
2020-05-06 15:15:41 +0100

    Tidy Up Layouts

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -15,72 +15,81 @@
  * @version 3.5.1
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 // Note: `wc_get_gallery_image_html` was added in WC 3.3.2 and did not exist prior. This check protects against theme overrides being used on older versions of WC.
-if ( ! function_exists( 'wc_get_gallery_image_html' ) ) {
-	return;
+if (!function_exists('wc_get_gallery_image_html')) {
+    return;
 }
 
 global $product;
 
-$columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
+$columns = apply_filters('woocommerce_product_thumbnails_columns', 4);
 $post_thumbnail_id = $product->get_image_id();
-$wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_classes', array(
-	'woocommerce-product-gallery',
-	'woocommerce-product-gallery--' . ( $product->get_image_id() ? 'with-images' : 'without-images' ),
-	'woocommerce-product-gallery--columns-' . absint( $columns ),
-	'images',
-) );
-$imageList         = [];
-$attachment_ids    = $product->get_gallery_image_ids();
-if ( $product->get_image_id() ) {
-	$imageList[] = wp_get_attachment_url( $post_thumbnail_id );
+$wrapper_classes = apply_filters('woocommerce_single_product_image_gallery_classes', array(
+    'woocommerce-product-gallery',
+    'woocommerce-product-gallery--' . ($product->get_image_id() ? 'with-images' : 'without-images'),
+    'woocommerce-product-gallery--columns-' . absint($columns),
+    'images',
+));
+$imageList = [];
+$attachment_ids = $product->get_gallery_image_ids();
+if ($product->get_image_id()) {
+    if (!in_array(wp_get_attachment_url($post_thumbnail_id), $imageList)) {
+        $imageList[] = wp_get_attachment_url($post_thumbnail_id);
+    }
+
 }
-foreach ( $attachment_ids as $attachment_id ) {
-	$imageList[] = wp_get_attachment_url( $attachment_id );
+foreach ($attachment_ids as $attachment_id) {
+    if (!in_array(wp_get_attachment_url($attachment_id), $imageList)) {
+        $imageList[] = wp_get_attachment_url($attachment_id);
+    }
 }
 ?>
-<div class="<?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>"
+<div class="<?php echo esc_attr(implode(' ', array_map('sanitize_html_class', $wrapper_classes))); ?>" data-columns="<?php echo esc_attr($columns); ?>"
      style="opacity: 0; transition: opacity .25s ease-in-out;">
     <figure class="woocommerce-product-gallery__wrapper">
-		<?php
-		if ( empty( $imageList ) ) {
+        <?php
+        if (empty($imageList)) {
 
-			$html = '<div class="woocommerce-product-gallery__image--placeholder">';
-			$html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
-			$html .= '</div>';
-			echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+            $html = '<div class="woocommerce-product-gallery__image--placeholder">';
+            $html .= sprintf('<img src="%s" alt="%s" class="wp-post-image" />', esc_url(wc_placeholder_img_src('woocommerce_single')), esc_html__('Awaiting product image', 'woocommerce'));
+            $html .= '</div>';
+            echo apply_filters('woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
 
-		} else {
-			?>
+        } else {
+            ?>
             <div class="slick-product-gallery">
-				<?php
-				foreach ( $imageList as $image ) {
-					?>
+                <?php
+                foreach ($imageList as $image) {
+                    ?>
                     <div class="slick-product-slide">
+                        <div class="product-image">
                             <img src="<?php echo $image; ?>" class="slick-product-image">
+                        </div>
                     </div>
-					<?php
-				}
-				?>
+                    <?php
+                }
+                ?>
             </div>
             <div class="slick-thumbnail-gallery">
-				<?php
-				foreach ( $imageList as $image ) {
-					?>
-                    <div class="slick-product-slide">
-
-                            <img src="<?php echo $image; ?>" class="slick-product-image">
-
-                    </div>
-					<?php
-				}
-				?>
+                <?php
+                if(count($imageList) > 1) {
+                    foreach ($imageList as $image) {
+                        ?>
+                        <div class="slick-product-slide">
+                            <div class="product-image">
+                                <img src="<?php echo $image; ?>" class="slick-product-image">
+                            </div>
+                        </div>
+                        <?php
+                    }
+                }
+                ?>
             </div>
-			<?php
-		}
-		?>
+            <?php
+        }
+        ?>
     </figure>
 </div>
 
