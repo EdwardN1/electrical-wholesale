1db84352a1f254fb2fe4ce53cfd99c56fb1b2c19
EdwardN1 <GH?841241ed>
2020-05-21 13:25:39 +0100

    iFrame Block

--- functions/includes/autoinc-block-iframe.php
+++ functions/includes/autoinc-block-iframe.php
@@ -1,15 +1,15 @@
 <?php
-add_action('acf/init', 'acfgbc_Block_CTA_Grid');
-function acfgbc_Block_CTA_Grid()
+add_action('acf/init', 'acfgbc_Block_iFrame');
+function acfgbc_Block_iFrame()
 {
     if (!function_exists('acf_register_block')) {
         return;
     }
     acf_register_block(array(
-        'name' => 'acfgbcBlock_CTA_Grid',
-        'title' => __('Block – CTA Grid'),
-        'description' => __('Block – CTA Grid'),
-        'render_callback' => 'acfgbc_Block_CTA_Grid_rc',
+        'name' => 'acfgbcBlock_iFrame',
+        'title' => __('Block – iFrame'),
+        'description' => __('Block – iFrame'),
+        'render_callback' => 'acfgbc_Block_iFrame_rc',
         'category' => 'technickswpwordpresstheme',
         'icon' => 'tagcloud',
         'mode' => 'preview',
@@ -18,10 +18,10 @@ function acfgbc_Block_CTA_Grid()
     ));
 }
 
-function acfgbc_Block_CTA_Grid_rc($block, $content = '', $is_preview = false)
+function acfgbc_Block_iFrame_rc($block, $content = '', $is_preview = false)
 {
     if ($is_preview) {
         include_once get_template_directory() . '/parts/blocks/editor/styles.php';
     }
-    include get_template_directory() . '/parts/blocks/BlockCTAGrid.php';
+    include get_template_directory() . '/parts/blocks/BlockiFrame.php';
 }
