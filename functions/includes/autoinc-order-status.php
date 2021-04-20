1f31796e9519aeeeabf2459a0b8793487309ccbf
Edward Nickerson <edward@technicks.com>
2021-04-20 20:40:38 +0100

    Ready for Collection Email

--- functions/includes/autoinc-order-status.php
+++ functions/includes/autoinc-order-status.php
@@ -1,5 +1,5 @@
 <?php
-// register a custom post status 'awaiting-delivery' for Orders
+// register a custom post status 'ready-for-collection' for Orders
 add_action( 'init', 'register_custom_post_status', 20 );
 function register_custom_post_status() {
     register_post_status( 'wc-ready-for-collection', array(
@@ -12,21 +12,21 @@ function register_custom_post_status() {
     ) );
 }
 
-// Adding custom status 'awaiting-delivery' to order edit pages dropdown
+// Adding custom status 'ready-for-collection' to order edit pages dropdown
 add_filter( 'wc_order_statuses', 'custom_wc_order_statuses', 20, 1 );
 function custom_wc_order_statuses( $order_statuses ) {
     $order_statuses['wc-ready-for-collection'] = _x( 'Ready for Collection', 'Order status', 'woocommerce' );
     return $order_statuses;
 }
 
-// Adding custom status 'awaiting-delivery' to admin order list bulk dropdown
+// Adding custom status 'ready-for-collection' to admin order list bulk dropdown
 add_filter( 'bulk_actions-edit-shop_order', 'custom_dropdown_bulk_actions_shop_order', 20, 1 );
 function custom_dropdown_bulk_actions_shop_order( $actions ) {
     $actions['mark_ready-for-collection'] = __( 'Mark Ready for Collection', 'woocommerce' );
     return $actions;
 }
 
-// Adding action for 'awaiting-delivery'
+// Adding action for 'ready-for-collection'
 add_filter( 'woocommerce_email_actions', 'custom_email_actions', 20, 1 );
 function custom_email_actions( $action ) {
     $actions[] = 'woocommerce_order_status_wc-ready-for-collection';
@@ -35,7 +35,7 @@ function custom_email_actions( $action ) {
 
 add_action( 'woocommerce_order_status_wc-ready-for-collection', array( WC(), 'send_transactional_email' ), 10, 1 );
 
-// Sending an email notification when order get 'awaiting-delivery' status
+// Sending an email notification when order get 'ready-for-collection' status
 add_action('woocommerce_order_status_ready-for-collection', 'backorder_status_custom_notification', 20, 2);
 function backorder_status_custom_notification( $order_id, $order ) {
     // HERE below your settings
@@ -46,9 +46,31 @@ function backorder_status_custom_notification( $order_id, $order ) {
     $mailer = WC()->mailer()->get_emails();
 
     // Customizing Heading and subject In the WC_email processing Order object
-    $mailer['WC_Email_Customer_Processing_Order']->heading = $heading;
-    $mailer['WC_Email_Customer_Processing_Order']->subject = $subject;
+    $mailer['WC_Ready_For_Collection_Email']->heading = $heading;
+    $mailer['WC_Ready_For_Collection_Email']->subject = $subject;
 
     // Sending the customized email
-    $mailer['WC_Email_Customer_Processing_Order']->trigger( $order_id );
+    $mailer['WC_Ready_For_Collection_Email']->trigger( $order_id );
 }
+
+/**
+ *  Add a custom email to the list of emails WooCommerce should load
+ *
+ * @since 0.1
+ * @param array $email_classes available email classes
+ * @return array filtered available email classes
+ */
+function add_ready_for_collection_woocommerce_email( $email_classes ) {
+
+    // include our custom email class
+    require( 'class-wc-ready-for-collection-email.php' );
+
+
+    // add the email class to the list of email classes that WooCommerce loads
+    $email_classes['WC_Ready_For_Collection_Email'] = new WC_Ready_For_Collection_Email();
+
+    return $email_classes;
+
+}
+add_filter( 'woocommerce_email_classes', 'add_ready_for_collection_woocommerce_email' );
+
---
+++ functions/includes/class-wc-ready-for-collection-email.php
@@ -0,0 +1,85 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * A custom Ready For Collection WooCommerce Email class
+ *
+ * @since 0.1
+ * @extends \WC_Email
+ */
+class WC_Ready_For_Collection_Email extends WC_Email {
+
+    /**
+     * Set email defaults
+     *
+     * @since 0.1
+     */
+    public function __construct() {
+
+        // set ID, this simply needs to be a unique name
+        $this->id = 'wc_ready_for_collection';
+
+        // this is the title in WooCommerce Email settings
+        $this->title = 'Ready for Collection';
+
+        // this is the description in WooCommerce email settings
+        $this->description = 'Ready for Collection Notification emails are sent when an order status is set to Ready for Collection';
+
+        // these are the default heading and subject lines that can be overridden using the settings
+        $this->heading = 'Your order is ready for collection';
+        $this->subject = 'Your order is ready for collection';
+
+        // these define the locations of the templates that this email should use, we'll just use the new order template since this email is similar
+        $this->template_html  = 'emails/customer-processing-order.php';
+        $this->template_plain = 'emails/plain/customer-processing-order.php';
+
+        // Trigger on new paid orders
+        //add_action( 'woocommerce_order_status_pending_to_processing_notification', array( $this, 'trigger' ) );
+        //add_action( 'woocommerce_order_status_failed_to_processing_notification',  array( $this, 'trigger' ) );
+
+        // Call parent constructor to load any other defaults not explicity defined here
+        parent::__construct();
+
+        // this sets the recipient to the settings defined below in init_form_fields()
+        //$this->recipient = $this->get_option( 'recipient' );
+        $this->recipient = 'Customer';
+
+        // if none was entered, just use the WP admin email as a fallback
+        if ( ! $this->recipient )
+            $this->recipient = get_option( 'admin_email' );
+    }
+
+    /**
+     * Determine if the email should actually be sent and setup email merge variables
+     *
+     * @since 0.1
+     * @param int $order_id
+     */
+    public function trigger( $order_id ) {
+
+        // bail if no order ID is present
+        if ( ! $order_id )
+            return;
+
+        // setup order object
+        $this->object = new WC_Order( $order_id );
+
+        // bail if shipping method is not expedited
+        //if ( ! in_array( $this->object->get_shipping_method(), array( 'Three Day Shipping', 'Next Day Shipping' ) ) )
+          //  return;
+
+        // replace variables in the subject/headings
+        $this->find[] = '{order_date}';
+        $this->replace[] = date_i18n( woocommerce_date_format(), strtotime( $this->object->order_date ) );
+
+        $this->find[] = '{order_number}';
+        $this->replace[] = $this->object->get_order_number();
+
+        if ( ! $this->is_enabled() || ! $this->get_recipient() )
+            return;
+
+        // woohoo, send the email!
+        $this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
+    }
+
+} // end \WC_Expedited_Order_Email class
